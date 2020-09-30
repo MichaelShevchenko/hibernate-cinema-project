3d4eb37cc74136a2e53b067309b6b83a935da49b
Mikhail Shevchenko <michaelyshevchenko@gmail.com>
2020-09-30 05:12:02 +0300

    configured project environment

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,12 @@
+import org.apache.log4j.Logger;
+
 public class Application {
+    private static final Logger LOGGER = Logger.getLogger(Application.class);
+
     public static void main(String[] args) {
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("This is debug message. Web server is up, application started.");
+        }
         System.out.println("Hello world");
     }
 }
