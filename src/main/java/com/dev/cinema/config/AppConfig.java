9291c4bb849b9191f9fe0c25b3ee0dd0408e13d6
Mikhail Shevchenko <michaelyshevchenko@gmail.com>
2020-10-22 10:59:55 +0300

    changed path for models in getSessionFactory() method

--- src/main/java/com/dev/cinema/config/AppConfig.java
+++ src/main/java/com/dev/cinema/config/AppConfig.java
@@ -42,7 +42,7 @@ public class AppConfig {
         properties.put("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
         properties.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
         factoryBean.setHibernateProperties(properties);
-        factoryBean.setPackagesToScan("com.hibernate.cinema.model");
+        factoryBean.setPackagesToScan("com.dev.cinema.model");
         return factoryBean;
     }
 }
