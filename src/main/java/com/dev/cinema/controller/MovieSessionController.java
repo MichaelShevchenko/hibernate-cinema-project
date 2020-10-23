acbc4f88dde310733a165ec11b9d976e3daf1065
Mikhail Shevchenko <michaelyshevchenko@gmail.com>
2020-10-23 16:13:20 +0300

    removed redundant impots

--- src/main/java/com/dev/cinema/controller/MovieSessionController.java
+++ src/main/java/com/dev/cinema/controller/MovieSessionController.java
@@ -7,13 +7,11 @@ import com.dev.cinema.service.MovieSessionService;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
